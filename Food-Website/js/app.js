5ae68b776907063569ea52696b5423b46735f9a1
Shubhajit Sarkar <43705275+shubhajit22@users.noreply.github.com>
2021-04-22 11:52:31 +0530

    slider and shadow fixed

--- Food-Website/js/app.js
+++ Food-Website/js/app.js
@@ -2,6 +2,9 @@ $(document).ready(function(){
     $('.food-slider').slick({
         slidesToShow:3,
         slidesToScroll:1,
+        autoplay:true,
+
+        // changing the default button by the custom one
         prevArrow:".prev-btn",
         nextArrow:".next-btn",
     });
